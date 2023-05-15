4e3c0249a470646a342b9166c1a91918dd7f7582
Nhlakanipho Zikhali <nlzikhali@gmail.com>
2023-05-15 23:35:49 +0200

    commit

--- RotativaConsole/RotativaConsole/Program.cs
+++ RotativaConsole/RotativaConsole/Program.cs
@@ -8,46 +8,21 @@ namespace RotativaConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
-            string resourceName = "RotativaConsole.File.test.html";
-            string fileContents = ReadEmbeddedResource(resourceName);
-
-
-
-
             string currentDirectory = Directory.GetCurrentDirectory();
 
-            //string htmlFile = Path.Combine(currentDirectory, "File", "test.html");
+            string htmlFile = Path.Combine(currentDirectory, "File", "test.html");
 
-            //string pdfFile = Path.Combine(currentDirectory, "File", "test.pdf");
+            string pdfFile = Path.Combine(currentDirectory, "File", "test.pdf");
 
             string workingDirectory = Path.Combine(currentDirectory, "Rotativa");
 
-            //string html = File.ReadAllText(htmlFile);
+            string html = File.ReadAllText(htmlFile);
 
-            byte[] pdfByteArr = WkhtmltopdfDriver.ConvertHtml("C:\\Projects\\RotativaConsole\\RotativaConsole\\Rotativa", "", fileContents);
+            byte[] pdfByteArr = WkhtmltopdfDriver.ConvertHtml(workingDirectory, "", html);
 
-            File.WriteAllBytes("RotativaConsole.File", pdfByteArr);
+            File.WriteAllBytes(pdfFile, pdfByteArr);
 
             Console.WriteLine("Pdf Created");
-        }
-
-        static string ReadEmbeddedResource(string resourceName)
-        {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            {
-                if (stream != null)
-                {
-                    using (StreamReader reader = new StreamReader(stream))
-                    {
-                        return reader.ReadToEnd();
-                    }
-                }
-            }
-
-            return "Resource not found.";
         }
     }
 }
